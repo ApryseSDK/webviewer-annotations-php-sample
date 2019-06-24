50cce393be434c71913f445cd5cf2d7600512dc2
Sardor Isakov <sisardor@gmail.com>
2019-06-24 16:18:52 -0700

    Update to WV 5.1

--- client/index.js
+++ client/index.js
@@ -1,18 +1,14 @@
 var viewerElement = document.getElementById('viewer');
-var viewer = new PDFTron.WebViewer({
-  path: 'lib',
-  initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
-}, viewerElement);
-var viewerInstance = null;
-var annotManager = null;
 var DOCUMENT_ID = 'webviewer-demo-1';
 
-viewerElement.addEventListener('ready', function() {
-  viewerInstance = viewer.getInstance();
-  annotManager = viewerInstance.docViewer.getAnnotationManager();
+WebViewer({
+  path: 'lib',
+  initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
+}, viewerElement).then(instance => {
+  var annotManager = instance.docViewer.getAnnotationManager();
 
   // Add a save button on header
-  viewerInstance.setHeaderItems(function(header) {
+  instance.setHeaderItems(function(header) {
     header.push({
       type: 'actionButton',
       img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
@@ -26,15 +22,17 @@ viewerElement.addEventListener('ready', function() {
       }
     });
   });
-});
 
-// Load annotations when document is loaded
-viewerElement.addEventListener('documentLoaded', function() {
-  loadXfdfString(DOCUMENT_ID).then(function(xfdfString) {
-    var annotations = annotManager.importAnnotations(xfdfString);
-    annotManager.drawAnnotationsFromList(annotations);
+
+  // Load annotations when document is loaded
+  instance.docViewer.on('documentLoaded', function() {
+    loadXfdfString(DOCUMENT_ID).then(function(xfdfString) {
+      var annotations = annotManager.importAnnotations(xfdfString);
+      annotManager.drawAnnotationsFromList(annotations);
+    });
   });
 });
+
 
 // Make a POST request with XFDF string
 var saveXfdfString = function(documentId, xfdfString) {
