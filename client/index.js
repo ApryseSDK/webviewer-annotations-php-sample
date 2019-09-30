fa9a8743060738b01ff1030f6c8e9ab04a4e79bc
achenPDFTron <achen@pdftron.com>
2019-09-30 09:45:30 -0700

    removed async/await

--- client/index.js
+++ client/index.js
@@ -12,12 +12,17 @@ WebViewer({
     header.push({
       type: 'actionButton',
       img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-      onClick: async function() {
+      onClick: function() {
         // Save annotations when button is clicked
         // widgets and links will remain in the document without changing so it isn't necessary to export them
-        var xfdfString = await annotManager.exportAnnotations({ links: false, widgets: false });
-        await saveXfdfString(DOCUMENT_ID, xfdfString);
-        alert('Annotations saved successfully.');
+        annotManager.exportAnnotations({ links: false, widgets: false })
+        .then(function (xfdfString) {
+          saveXfdfString(DOCUMENT_ID, xfdfString);
+          alert('Annotations saved successfully.');
+        })
+        .catch(function () {
+          alert('Annotations not saved successfully.');
+        });
       }
     });
   });
